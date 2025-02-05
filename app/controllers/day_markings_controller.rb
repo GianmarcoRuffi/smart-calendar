7c93eb530c62a3e9537f6735e48063288e0d802e
Gianmarco Ruffi <g.ruffi@esis-italia.com>
2025-02-05 09:40:39 +0100

    main: Updated click event

--- app/controllers/day_markings_controller.rb
+++ app/controllers/day_markings_controller.rb
@@ -1,19 +1,23 @@
 class DayMarkingsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def toggle
     date = Date.parse(params[:date])
+
+    # Crea o distrugge la marcatura
     marking = current_user.day_markings.find_by(marked_on: date)
-    
     if marking
       marking.destroy
     else
       current_user.day_markings.create(marked_on: date)
     end
 
-    # Dopo aver modificato le marcature, ricalcola l'HTML della sezione "markings"
-    # Puoi renderizzare un partial (che creiamo nel prossimo punto) e restituirlo come stringa
-    markings_html = render_to_string(partial: 'calendars/markings', locals: { date: date, day_markings: @day_markings })
-    render json: { html: markings_html }
+    # Recupera le marcature per il giorno
+    @day_markings = DayMarking.all
+
+    # Risponde con il nuovo stato delle marcature in formato JSON
+    respond_to do |format|
+      format.json { render json: { html: render_to_string(partial: 'calendars/markings', locals: { date: date }) } }
+    end
   end
 end
